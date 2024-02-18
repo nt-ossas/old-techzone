611001dcff69ec1916d2c1fae5802430ff14712f
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-02-18 22:01:26 +0100

    nav

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var testi = document.getElementById("ciao");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 function dark(){
     alert("Funzione ancora non disponibile, torna a controllare più tardi");
     testi.classList.toggle("body");
 };
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
