9452416857b6f1963b69cd5297febf61ba077e86
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-03-05 15:29:00 +0100

    nav

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 var ciao = document.querySelector(".body");
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
 
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-});
 
-function dark(){
-    ciao.classList.remove('.body');
-    ciao.classList.add('.darkmode');
-};
 function dontwork(){
     alert("Funzione ancora in fase di sviluppo, torna a controllare più tardi");
 };
