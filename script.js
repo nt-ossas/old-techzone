72886d32134c5e545954d1563337b0e002fc4aec
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-03-05 17:33:56 +0100

    BIG up

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var ciao = document.querySelector(".body");
+function dark(){
+    const bodyElements = document.querySelectorAll('.body');
+    const liElements = document.querySelectorAll('li');
 
-
-
+    liElements.forEach((element) => {
+    element.classList.toggle('corn');
+    });
+    bodyElements.forEach((element) => {
+    element.classList.toggle('darkmode');
+    });
+};
 function dontwork(){
     alert("Funzione ancora in fase di sviluppo, torna a controllare più tardi");
 };
