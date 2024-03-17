8554112d34667b687134c86db30f49b67de034ce
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-03-17 12:30:44 +0100

    big

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function dark(){
     const bodyElements = document.querySelectorAll('.body');
     const liElements = document.querySelectorAll('li');
+    const luna = document.getElementById( 'luna' );
+    const sole = document.getElementById( 'sole' );
+
+    // Change the class of the elements
+    luna.classList.toggle('hidden');
+    sole.classList.toggle('hidden');
     document.querySelector('body').classList.toggle('black');
     liElements.forEach((element) => {
     element.classList.toggle('corn');
