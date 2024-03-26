d6f388f1f039248934ac1b4d55874ab45c2258f9
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-03-27 00:15:09 +0100

    ciuao

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ hamburger.addEventListener('click', ()=>{
 function dontwork(){
     alert("Funzione ancora in fase di sviluppo, torna a controllare più tardi");
 };
+
+function tornasu(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
