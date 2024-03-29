25b6bfa74110baed0cc301b836fcaf9a0c320fb2
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-03-29 12:57:45 +0100

    food

---
+++ js/new.js
@@ -0,0 +1,144 @@
+function mostraRisultato() {
+    var dove = document.getElementById("dove").value;
+    var appetito = document.getElementById("appetito").value;
+    var orario = document.getElementById("orario").value;
+    const stampa = document.getElementById("stampa");
+
+    //fuori poca
+    if(dove === "fuori" && appetito === "poca"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi andare a fare colazione al bar vicino a casa !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi andare a magiare un panino al chioschetto !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi andare a magiare un menù small con il POLLO con le offerte del mc !";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi andare a prendere qualcosa al bar !";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+    //fuori abbastanza
+    if(dove === "fuori" && appetito === "abbastanza"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi andare a prendere una bella pasta al bar vicino a casa !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi andare a magiare una piadina al chioschetto !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi andare a magiare un menù medio con il POLLO con le offerte del mc !";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi andare a prendere qualcosa al bar !";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+    //fuori tanta
+    if(dove === "fuori" && appetito === "tanta"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi andare a fare colazione al mc/burger king !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi andare a mangiare un menù large con il POLLO con le offerte del mc !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi andare a magiare una bella pizza !";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi andare a prendere qualcosa al bar !";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+    //casa poca
+    else if(dove === "casa" && appetito === "poca"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi mangiarti un po' di fragole !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi farti un panino con il POLLO !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi farti una bella piadina (mozzarella) !";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi mangiarti un gelato !";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+    //casa poca
+    else if(dove === "casa" && appetito === "poca"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi mangiarti un po' di fragole !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi farti un panino con il POLLO !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi farti una bella piadina (mozzarella) !";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi mangiarti un gelato !";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+    //casa abbastanza
+    else if(dove === "casa" && appetito === "abbastanza"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi mangiarti una ciotola di fragole !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi mangiarti un wrap con il POLLO !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi magiarti un paninozzo con il POLLO !";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi mangiarti un po' di fragole !";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+    //casa tanta
+    else if(dove === "casa" && appetito === "abbastanza"){
+        if(orario === "mattina") {
+            stampa.textContent = "Puoi mangiarti una ciotola di fragole con il succo al mirtillo !";
+        } 
+        else if(orario === "pranzo") {
+            stampa.textContent = "Puoi mangiarti un wrap con il POLLO con patatine !";
+        }
+        else if(orario === "cena") {
+            stampa.textContent = "Puoi magiarti un paninozzo con il POLLO, bacon e patatine!";
+        }
+        else if(orario === "snack") {
+            stampa.textContent = "Puoi mangiarti un po' di fragole con il succo al mirtillo!";
+        }
+        else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+        }
+    }
+   else{
+            stampa.textContent = "Seleziona luogo appetito e orario correttamente !";
+            alert("Seleziona luogo appetito e orario correttamente !");
+    }
+}
