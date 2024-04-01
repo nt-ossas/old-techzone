18ed2a0579294c4f4790feb52000fe54019750f5
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-04-02 00:53:09 +0200

    mf

---
+++ MF/mf.js
@@ -0,0 +1,7 @@
+function dontwork(){
+    alert("Funzione ancora in fase di sviluppo, torna a controllare più tardi");
+};
+
+function tornasu(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
