10270cbb56aefd3aefb8d7ab512a6f3d85228479
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-04-03 16:58:26 +0200

    prova

--- MF/mf.js
+++ MF/mf.js
@@ -5,3 +5,57 @@ function dontwork(){
 function tornasu(){
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+let dropdowns = document.querySelectorAll('.navbar .dropdown-toggler')
+let dropdownIsOpen = false
+
+// Handle dropdown menues
+if (dropdowns.length) {
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', (event) => {
+      let target = document.querySelector(`#${event.target.dataset.dropdown}`)
+
+      if (target) {
+        if (target.classList.contains('show')) {
+          target.classList.remove('show')
+          dropdownIsOpen = false
+        } else {
+          target.classList.add('show')
+          dropdownIsOpen = true
+        }
+      }
+    })
+  })
+}
+
+// Handle closing dropdowns if a user clicked the body
+window.addEventListener('mouseup', (event) => {
+  if (dropdownIsOpen) {
+    dropdowns.forEach((dropdownButton) => {
+      let dropdown = document.querySelector(`#${dropdownButton.dataset.dropdown}`)
+      let targetIsDropdown = dropdown == event.target
+
+      if (dropdownButton == event.target) {
+        return
+      }
+
+      if ((!targetIsDropdown) && (!dropdown.contains(event.target))) {
+        dropdown.classList.remove('show')
+      }
+    })
+  }
+})
+function handleSmallScreens() {
+  document.querySelector('.navbar-toggler')
+    .addEventListener('click', () => {
+      let navbarMenu = document.querySelector('.navbar-menu')
+
+      if (!navbarMenu.classList.contains('active')) {
+        navbarMenu.classList.add('active')
+      } else {
+        navbarMenu.classList.remove('active')
+      }
+    })
+}
+
+handleSmallScreens()
